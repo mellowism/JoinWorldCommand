a2c6ae92de0a0c4bbdfe60c7a0d9f0517f915a61
BuildTools <unconfigured@null.spigotmc.org>
2019-04-24 18:13:03 +0200

    Sending message to player.

--- src/main/java/com/mellowism/jwc/joinworldcommand/JoinWorldCommand.java
+++ src/main/java/com/mellowism/jwc/joinworldcommand/JoinWorldCommand.java
@@ -1,5 +1,6 @@
 package com.mellowism.jwc.joinworldcommand;
 
+import com.mellowism.jwc.joinworldcommand.events.JoinWorld;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +10,9 @@ public final class JoinWorldCommand extends JavaPlugin {
     public void onEnable() {
         //When server starts, print this message:
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "JoinWorldCommand " + ChatColor.GREEN + "enabled!");
+
+        //This is for enabling my events:
+        getServer().getPluginManager().registerEvents(new JoinWorld(), this);
     }
 
     @Override
