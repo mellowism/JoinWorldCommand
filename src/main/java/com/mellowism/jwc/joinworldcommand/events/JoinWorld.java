f754ef6c9c53734d1fc432a284a05a90505cd8ec
BuildTools <unconfigured@null.spigotmc.org>
2019-04-24 21:20:36 +0200

    Managed to get the command from config file and executed it as player.

--- src/main/java/com/mellowism/jwc/joinworldcommand/events/JoinWorld.java
+++ src/main/java/com/mellowism/jwc/joinworldcommand/events/JoinWorld.java
@@ -1,10 +1,15 @@
 package com.mellowism.jwc.joinworldcommand.events;
 
+import com.mellowism.jwc.joinworldcommand.JoinWorldCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.plugin.Plugin;
+
 
 /**
  * com.mellowism.jwc.joinworldcommand.events was created by mellowism on 24-Apr-19.
@@ -12,6 +17,8 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
  * www.mellowism.com (mellow@mellowism.com)
  */
 public class JoinWorld implements Listener {
+
+    Plugin plugin = JoinWorldCommand.getPlugin(JoinWorldCommand.class);
 
     @EventHandler
 
@@ -21,13 +28,16 @@ public class JoinWorld implements Listener {
         Player player = e.getPlayer();
 
         //get the world name
-        String world = e.getPlayer().getWorld().getName();
+        //String world = e.getPlayer().getWorld().getName();
 
         //send the player a message when changing world
-        player.sendMessage("Hello " + ChatColor.GREEN + world);
+        //player.sendMessage("Hello " + ChatColor.GREEN + world);
 
-        //perform command when changing world
-        player.performCommand("ttest");
+        //Player perform command when changing world
+        player.performCommand(plugin.getConfig().getString("world.command"));
+
+        //Console perform command when changing world
+        //Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),"")
 
 
     }
